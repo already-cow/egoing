6cf6759d069a4bfa279585645979e36d429e9145
alreadycow <alreadycow_@naver.com>
2023-10-10 17:33:27 +0900

    Hello World

---
+++ HelloWorld/src/ClassApp.java
@@ -0,0 +1,14 @@
+
+public class ClassApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(1.6));
+		System.out.println(Math.ceil(1.6));
+		
+		
+		
+	}
+
+}
